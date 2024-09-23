88791570bcc176ae740486a2bad3d167d902f7da
Md. Saiful Islam <73848027+saifpust@users.noreply.github.com>
2024-09-23 20:59:24 +0600

    header done

---
+++ assets/js/sb-scripts.js
@@ -0,0 +1,25 @@
+
+// Header sticky 
+const common_header = document.querySelector(".sb-header");
+
+if(common_header) {
+
+    const common_header_height = common_header.clientHeight;
+    const common_header_top_space = common_header.offsetTop;
+    const hamburger_menu_icon = document.querySelector(".hamburger-menu");
+
+    hamburger_menu_icon.addEventListener("click", function() {
+        common_header.classList.toggle("sb-mobile-menu-active");
+    });
+
+    window.onscroll = function() {
+        if (window.scrollY > 0) {
+            common_header.classList.add("sticky");
+        } else {
+            common_header.classList.remove("sticky");
+        }
+    };
+
+    document.body.style.setProperty('--header-height', common_header_height + 'px');
+    document.body.style.setProperty('--header-top-space', common_header_top_space + 'px');
+};
