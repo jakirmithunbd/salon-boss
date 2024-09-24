372935daa9bffc27dd4d7ac3d3865abb95fde1ed
Md. Saiful Islam <73848027+saifpust@users.noreply.github.com>
2024-09-24 18:10:04 +0600

    icon box sectoin done

--- assets/js/sb-scripts.js
+++ assets/js/sb-scripts.js
@@ -23,3 +23,33 @@ if(common_header) {
     document.body.style.setProperty('--header-height', common_header_height + 'px');
     document.body.style.setProperty('--header-top-space', common_header_top_space + 'px');
 };
+
+
+// Service Slider 
+$('.sb-service-slider-wrapper').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 4,
+    autoplay: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
