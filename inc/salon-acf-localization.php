7a478f8a2ff4e12769e594e9b5e0307e02125114
Jakirm <jakirmithunbd@gmail.com>
2024-10-03 17:59:20 +0600

    Update ACF load sync

--- inc/salon-acf-localization.php
+++ inc/salon-acf-localization.php
@@ -4,3 +4,14 @@ function salon_acf_json_save_point( $path ) {
     return get_theme_file_path('/acf-fields');
 }
 add_filter( 'acf/settings/save_json', 'salon_acf_json_save_point' );
+
+function my_acf_json_load_point( $paths ) {
+    // Remove the original path (optional).
+    unset( $paths[0] );
+
+    // Append the new path and return it.
+    $paths[] = get_stylesheet_directory() . '/acf-fields';
+
+    return $paths;
+}
+add_filter( 'acf/settings/load_json', 'my_acf_json_load_point' );
