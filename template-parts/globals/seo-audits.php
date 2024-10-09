c94f6d37d9a3f0320c01049b27c7a0c78f928b36
Jakirm <jakirmithunbd@gmail.com>
2024-10-09 12:14:13 +0600

    Fix SEO Audit template - Global - options page

--- template-parts/globals/seo-audits.php
+++ template-parts/globals/seo-audits.php
@@ -1,14 +1,10 @@
 <?php
 $seo_audits_cta = get_field('seo_audits_cta', 'options');
 
-$seo_title = isset($seo_audits_cta['title']) ? esc_html($seo_audits_cta['title']) : '';
-$seo_sub_title = isset($seo_audits_cta['sub_title']) ? esc_html($seo_audits_cta['sub_title']) : '';
-$seo_sub_description = isset($seo_audits_cta['description']) ? esc_html($seo_audits_cta['description']) : '';
-$service_links = isset($seo_audits_cta['service_links']) ? $seo_audits_cta['service_links'] : site_url();
-$service_img = isset($seo_audits_cta['image']['url']) ? $seo_audits_cta['image']['url'] : get_theme_file_uri('/assets/images/Placeholder Image.svg');
-$service_img_title = isset($seo_audits_cta['image']['title']) ? esc_attr($seo_audits_cta['image']['title']) : 'Salon Boss Image';
+
 $image_alignment = isset($seo_audits_cta['image_alignment']['value']) ? esc_attr($seo_audits_cta['image_alignment']['value']) : '';
 $website_link = isset($seo_audits_cta['website_link']) ? $seo_audits_cta['website_link'] : site_url();
+
 ?>
 
 <section class="sb-free-seo-audit">
@@ -17,15 +13,27 @@ $website_link = isset($seo_audits_cta['website_link']) ? $seo_audits_cta['websit
             <div class="sb-card sb-card-filled-bg <?php echo $image_alignment; ?>">
                 <div class="sb-card-contents-wrapper d-flex align-center">
                     <div class="sb-card-image d-flex">
-                        <img src="<?php echo esc_url($service_img); ?>" alt="<?php echo $service_img_title; ?>">
+                    <?php if($seo_audits_cta['image']['url']) : ?>
+                        <img src="<?php echo esc_url($seo_audits_cta['image']['url']); ?>" alt="<?php echo $seo_audits_cta['image']['title']; ?>">
+                    <?php endif;?>
                     </div>
                     <div class="sb-card-content text-center">
 
-                        <h2><?php echo wp_kses_post($seo_title); ?> </h2>
-                        <h5><?php echo wp_kses_post($seo_sub_title); ?></h5>
-                        <p><?php echo wp_kses_post($seo_sub_description); ?></p>
+                        <?php if($seo_audits_cta['title']) {
+                            printf('<h2>%s</h2>', wp_kses_post($seo_audits_cta['title']));
+                        } ?>
 
-                        <?php if (!empty($service_links)) : ?>
+                        <?php if($seo_audits_cta['sub_title']) {
+                            printf('<h5>%s</h5>', wp_kses_post($seo_audits_cta['sub_title']));
+                        } ?>
+
+                        <?php if($seo_audits_cta['description']) {
+                            printf('<p>%s</p>', wp_kses_post($seo_audits_cta['description']));
+                        } ?>
+
+                        <?php
+                        $service_links = $seo_audits_cta['service_links'];
+                        if (!empty($service_links)) : ?>
                             <ul class="unstyle flex-center flex-wrap">
                                 <?php foreach ($service_links as $link) : ?>
                                     <li class="active">
