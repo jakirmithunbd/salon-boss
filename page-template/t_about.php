9208f09797c35f182871b26b447900c458f85221
mehedy-alamin <mehedy.codeconfig@gmail.com>
2024-10-10 12:24:56 +0600

    salon single blog page need check

--- page-template/t_about.php
+++ page-template/t_about.php
@@ -1,7 +1,10 @@
 <?php
 /*
-*  Template name: About Us
-* */
+ *  Template name: About Us
+ * */
 get_header(); ?>
+
+<?php get_template_part('template-parts/page-banner'); ?>
+
 
 <?php get_footer(); ?>
