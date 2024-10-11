58d16c115f98e94cf65b11bef70c7f8f1cb6cfd7
Tanin Ahmed <taninakond@gmail.com>
2024-10-11 15:43:46 +0600

    add case study counter suffix and prefix

--- template-parts/case-study/counter.php
+++ template-parts/case-study/counter.php
@@ -1,11 +1,34 @@
 <section class="sb-case-study-counter">
     <div class="container">
         <div class="sb-counter-list d-flex flex-wrap">
-            <?php $box_items = get_sub_field('box_items'); if(!empty($box_items)) : foreach ($box_items as $item) : ?>
+            <?php $box_items = get_sub_field('box_items');
+            if(!empty($box_items)) : foreach ($box_items as $item) : ?>
             <div class="sb-counter-item">
-                <?php if($item['number_text']) {
-                    printf('<h2><span class="sb-counter-amount" data-target="%s">0</span></h2>', wp_kses_post($item['number_text']));
-                }?>
+            <?php
+            if (!empty($item['number_text'])) {
+                $explode_number = explode('|', $item['number_text']);
+
+                if (count($explode_number) === 1 && is_numeric($item['number_text'])) {
+                    printf(
+                        '<h2><span class="sb-counter-amount" data-target="%s">0</span></h2>',
+                        esc_html($item['number_text'])
+                    );
+                } elseif (count($explode_number) === 2 && is_numeric($explode_number[0])) {
+                    printf(
+                        '<h2><span class="sb-counter-amount" data-target="%s">0</span><span class="sb-counter-suffix">%s</span></h2>',
+                        esc_html($explode_number[0]),
+                        esc_html($explode_number[1])
+                    );
+                } elseif (count($explode_number) === 2 && is_numeric($explode_number[1])) {
+                    printf(
+                        '<h2><span class="sb-counter-prefix">%s</span><span class="sb-counter-amount" data-target="%s">0</span></h2>',
+                        esc_html($explode_number[0]),
+                        esc_html($explode_number[1])
+                    );
+                }
+            }
+                ?>
+
 
                 <?php if($item['heading']) {
                     printf('<h3>%s</h3>', wp_kses_post($item['heading']));
