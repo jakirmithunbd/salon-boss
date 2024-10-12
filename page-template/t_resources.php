aea5810dbdc8b992e8f465e76a773a66e3504090
mehedy-alamin <mehedy.codeconfig@gmail.com>
2024-10-12 18:14:30 +0600

    back

--- page-template/t_resources.php
+++ page-template/t_resources.php
@@ -7,22 +7,17 @@ get_header(); ?>
 
 <section class="sb-resource">
     <div class="container">
-        <?php
-
-
-        ; ?>
         <div class="sb-resource-list d-flex flex-wrap">
-
             <?php
             $resource_list = get_field('resource_list');
             if ($resource_list):
                 foreach ($resource_list as $list):
 
-                    $resource_list_title = $list['title'];
-                    $resource_list_sub_title = $list['sub_title'];
-                    $resource_list_image = $list['image'];
-                    $resource_list_button = $list['button'];
-                    $resource_list_keypoints = $list['keypoint'];
+                    $resource_list_title = $list['title'] ?? '';
+                    $resource_list_sub_title = $list['sub_title'] ?? '';
+                    $resource_list_image = $list['image'] ?? '';
+                    $resource_list_button = $list['button'] ?? '';
+                    $resource_list_keypoints = $list['keypoint'] ?? '';
                     $resource_list_image_position = $list['image_position'] ?? '';
 
                     ; ?>
@@ -31,36 +26,34 @@ get_header(); ?>
                         <!-- image-position-right / image-position-top / image-position-top-left / image-position-top-right -->
                         <div class="sb-card-contents-wrapper d-flex align-center">
                             <div class="sb-card-image d-flex">
-                                <a href="./single-service.html">
+                                <a href="<?php echo esc_url($resource_list_button['url'] ?? site_url()); ?>">
                                     <img src="<?php echo esc_url($resource_list_image['url'] ?? ''); ?>"
                                         alt="<?php echo esc_attr($resource_list_image['alt'] ?? ''); ?>">
                                 </a>
                             </div>
                             <div class="sb-card-content text-center">
-                                <a class="sb-resource-title" href="./single-service.html">
-                                    <h2>Live <span>Webinars</span> 👩‍💻</h2>
+                                <a class="sb-resource-title"
+                                    href="<?php echo esc_url($resource_list_button['url'] ?? site_url()); ?>">
+                                    <h2><?php echo wp_kses_post($resource_list_title ?? ''); ?></h2>
                                 </a>
-                                <h5>Custom Websites That Convert</h5>
+                                <h5><?php echo esc_attr($resource_list_sub_title ?? ''); ?></h5>
 
                                 <?php
                                 if ($resource_list_keypoints):
                                     ?>
                                     <ul class="unstyle flex-center flex-wrap">
                                         <?php
-                                        $audit_keypoint = $audit_service_keypoints['keypoint'];
-
-                                        if ($audit_keypoint):
-                                            foreach ($audit_keypoint as $keypoint):
-                                                ?>
-                                                <li><?php echo wp_kses_post($keypoint['keypoint_text'] ?? ""); ?></li>
-                                                <?php
-                                            endforeach;
-                                        endif;
+                                        foreach ($resource_list_keypoints as $keypoint):
+                                            ?>
+                                            <li><a href="#"><?php echo wp_kses_post($keypoint['keypoint_text'] ?? ""); ?></a></li>
+                                            <?php
+                                        endforeach;
                                         ?>
                                     </ul>
                                 <?php endif; ?>
                                 <div class="sb-card-btn">
-                                    <a href="./single-service.html">Explore Our Website Services ></a>
+                                    <a
+                                        href="<?php echo esc_url($resource_list_button['url'] ?? site_url()); ?>"><?php echo esc_attr($resource_list_button['title'] . '>'); ?></a>
                                 </div>
                             </div>
                         </div>
@@ -71,5 +64,38 @@ get_header(); ?>
 
     </div>
 </section><!-- SB Resource  -->
+
+
+<section class="sb-content-resource">
+    <div class="container">
+        <div class="sb-content-resource-list d-flex flex-wrap">
+
+            <div class="sb-content-reource-item">
+                <a href="#" class="d-flex">
+                    <img src="../assets/images/SalonBoss-Ai-Logo.png" alt="" />
+                </a>
+                <a class="sb-content-resource-title" href="#">
+                    <h3>Ai Assistant For Beauty Professionals</h3>
+                </a>
+                <p>
+                    Salon Boss Ai, is a chatbot assistant that can help
+                    you with your online marketing.
+                </p>
+                <ul>
+                    <li>
+                        <a href="#">Like ChatGPT But Cheaper & Tailored for
+                            Beauty Professionals</a>
+                    </li>
+                </ul>
+                <div class="sb-content-reource-item-foot">
+                    <a href="#">Learn More</a>
+                </div>
+            </div>
+            <!-- Content Resource Item  -->
+
+        </div>
+    </div>
+</section>
+<!-- Sb Content Resource -->
 
 <?php get_footer(); ?>
