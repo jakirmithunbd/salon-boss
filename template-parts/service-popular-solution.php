f2abda8dda87a765a5bf5b4e368584d8f39a7e9e
Jakirm <jakirmithunbd@gmail.com>
2024-10-15 10:52:21 +0600

    Fixing service single page issues

--- template-parts/service-popular-solution.php
+++ template-parts/service-popular-solution.php
@@ -17,13 +17,13 @@ $solution_list = get_sub_field('solution_list');
         <div class="sb-reputation-review-list d-flex flex-wrap">
 
             <?php if (!empty($solution_list)) : foreach ($solution_list as $list) :
-                $image = $list['image']['url']
-                    ?? $list['image']['url']
-                    ?? esc_url(get_theme_file_uri('/assets/images/Placeholder Image.svg'));
+                $image = $list['image']
+                    ? $list['image']['url']
+                    : esc_url(get_theme_file_uri('/assets/images/Placeholder Image.svg'));
                 ?>
 
             <div class="sb-reputation-review-item">
-                <img src="<?php echo $image; ?>>" alt="">
+                <img src="<?php echo $image; ?>" alt="">
                 <h3><?php echo $list['title'];?></h3>
                 <p>
                     <?php echo $list['description'];?>
