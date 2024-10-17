0f58879427126805757333793497e984665dc6f6
Md. Saiful Islam <73848027+saifpust@users.noreply.github.com>
2024-10-17 11:30:05 +0600

    working on parent template

---
+++ page-template/parent-template.php
@@ -0,0 +1,29 @@
+
+<?php
+/*
+ *  Template name: Parent Template
+ * */
+get_header(); ?>
+
+<?php get_template_part('template-parts/page-banner'); ?>
+
+<section class="why-sb-parent-service">
+    <div class="container">
+        <div class="parent-section-title">
+            <h2>Why Business Listings Matter?</h2>
+        </div>
+    </div>
+</section>
+<!-- Why Parent service  -->
+
+
+
+
+
+<?php
+get_template_part('template-parts/service-booking-cta');
+
+get_template_part('template-parts/service-resource-center');
+
+
+get_footer(); ?>
