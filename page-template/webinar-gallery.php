5e0127381e5daf3ab4c2fe5666cb5167c46a5102
Md. Saiful Islam <73848027+saifpust@users.noreply.github.com>
2024-10-18 15:05:24 +0600

    working on webinar gallery

---
+++ page-template/webinar-gallery.php
@@ -0,0 +1,20 @@
+
+<?php
+/*
+*  Template name: Webinar Gallery
+* */
+get_header();
+get_template_part('template-parts/page-banner');
+?>
+
+
+
+
+
+
+
+
+
+<?php get_template_part('template-parts/service-resource-center'); ?>
+
+<?php get_footer(); ?>
